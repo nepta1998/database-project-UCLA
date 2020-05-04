30f7b94a773d36ad03a8e9c1c0ec0a05bcdf0d5f
Neptalí Piña <njpm28@gmail.com>
2020-05-04 12:54:58 -0400

    pagina principal complete

--- app.py
+++ app.py
@@ -1,11 +1,61 @@
-from flask import Flask
+import sqlite3
+from flask import (
+    Flask,
+    g,
+    render_template
+)
 
 app = Flask(__name__)
+DATABASE = 'database.db'
+
+
+def make_dicts(cursor, row):
+    """
+    Muestra los datos obtenidos de la base de datos
+    en forma de diccionario en lugar de tupla.
+    """
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
+def get_db():
+    """Devuelve la conección de la base de datos"""
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = make_dicts
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    """Cirra la conección cuando termina el request context"""
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    """Ejecuta consultas SELECT en la base de datos"""
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 
 @app.route("/")
 def index():
-    return "hello world"
+    return render_template('index.html')
+
+
+@app.route("/eventos")
+def events():
+    return "eventos"
+
+
+@app.route("/registrar-reserva")
+def reserve_register():
+    return "registrar reserva"
 
 
 if __name__ == "__main__":
